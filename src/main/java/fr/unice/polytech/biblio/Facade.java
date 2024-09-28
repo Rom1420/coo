5318bbe8f43c298b842cad79c48d27ec856a9d93
YohanSI3 <yohan.mazzi@etu.unice.fr>
2024-09-28 19:15:45 +0200

    Déclaration de la classe Facade #5

---
+++ src/main/java/fr/unice/polytech/biblio/Facade.java
@@ -0,0 +1,33 @@
+package fr.unice.polytech.biblio;
+
+public class Facade {
+
+    //private OrderManager orderManager;
+    //private GroupOrderManager groupOrderManager;
+    private RestaurantManager restaurantManager;
+
+    public void consultRestaurants () {
+        return;
+    }
+
+    public void placeOrder (){
+        return;
+    }
+
+    public void payOrder () {
+        return;
+    }
+
+    public void updateService () {
+        return;
+    }
+
+    public void createGroup () {
+        return;
+    }
+
+    public void joinGroup() {
+        return;
+    }
+
+}
