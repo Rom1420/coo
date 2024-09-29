69153d0eca7fe8773bf9aef3f55ff48dbd259fac
noraKay <nora.k@outlook.fr>
2024-09-29 10:38:29 +0200

    Implémentation de la gestion des utilisateurs #8

---
+++ src/main/java/fr/unice/polytech/biblio/RegisteredUser.java
@@ -0,0 +1,18 @@
+package fr.unice.polytech.biblio;
+
+public class RegisteredUser extends UserAccount {
+    private boolean isGroupMember;
+
+    public RegisteredUser(String name, int id, String password) {
+        super(name, id, password);
+        this.isGroupMember = false;
+    }
+
+    public boolean isGroupMember() {
+        return isGroupMember;
+    }
+
+    public void setGroupMember(boolean isGroupMember) {
+        this.isGroupMember = isGroupMember;
+    }
+}
