aaa03d5f4073142428c8184eed4549d2f5051d42
YohanSI3 <yohan.mazzi@etu.unice.fr>
2024-10-19 19:38:17 +0200

    Bug méthode en 2 fois corrigé

--- src/test/java/fr/unice/polytech/restaurant/RManagerUpdateTime.java
+++ src/test/java/fr/unice/polytech/restaurant/RManagerUpdateTime.java
@@ -45,11 +45,6 @@ public class RManagerUpdateTime {
         user2 = new UserAccount(string, int1, "2222");
     }
 
-    @When("{string} specifies a preparation time of {int} seconds per person for the {string}")
-    public void specifies_a_preparation_time_of_seconds_per_person_for_the(String string, Integer int1, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
-    }
 
     @Then("they can produce up to {int} portions of fires")
     public void they_can_produce_up_to_portions_of_fires(Integer int1) {
@@ -114,6 +109,7 @@ public class RManagerUpdateTime {
         }
         assertTrue(isPresentInList);
     }
+
     @When("{string} specifies a preparation time of {int} minute per person for the {string}")
     public void specifies_a_preparation_time_of_seconds_per_person_for_the(String string, Integer int1, String string2) {
         for(Article art : restaurant.getArticlesSimples()){
