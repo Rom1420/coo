20e13b0c447a15119150b6d3c64bf2c87f834b3e
YohanSI3 <yohan.mazzi@etu.unice.fr>
2024-10-20 14:32:29 +0200

    feature pour l'US créer un groupe #36

--- src/test/java/fr/unice/polytech/steats/GroupOrderCreationStepdefs.java
+++ src/test/java/fr/unice/polytech/steats/GroupOrderCreationStepdefs.java
@@ -2,59 +2,114 @@ package fr.unice.polytech.steats;
 
 import fr.unice.polytech.order.GroupOrder;
 import fr.unice.polytech.order.GroupOrderImpl;
+import fr.unice.polytech.order.GroupOrderManager;
 import fr.unice.polytech.order.Order;
 import fr.unice.polytech.restaurant.Restaurant;
 import fr.unice.polytech.user.RegisteredUser;
+import fr.unice.polytech.user.RegisteredUserManager;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.mockito.internal.matchers.Or;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class GroupOrderCreationStepdefs {
 
     RegisteredUser user;
-    GroupOrderImpl groupOrder;
-    Restaurant restaurant;
+    RegisteredUserManager registeredUserManager = RegisteredUserManager.getRegisteredUserManagerInstance();
+    GroupOrderManager groupOrderManager = GroupOrderManager.getGroupOrderManagerInstance();
+    GroupOrderImpl groupOrder = new GroupOrderImpl(0);
+    Restaurant restaurant = new Restaurant("Restau");
+    String deliveryLocalisation;
     Date deliveryTime;
-    String deliveryLocation;
-    Integer groupId;
-    Order order = new Order(new Date(), deliveryTime, deliveryLocation, new Restaurant("Restau"));
+    Order order;
+    int nbOfGroupOrderBeforeCreation;
 
-    @Given("a registered user of name Paul with id {int}")
-    public void a_registered_user_of_name_paul_with_id(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        user = new RegisteredUser("Paul", int1, "pp");
+    @Given("a registered user {string} with id {int}")
+    public void a_registered_user_of_name_paul_with_id(String string, Integer int1) {
+        user = new RegisteredUser(string, int1, "pp");
+        registeredUserManager.addNewRegisteredUser(user);
     }
 
-    @When("{string} wants to create a group order")
-    public void wants_to_create_a_group_order(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.println("Paul is creating a group !");
+    @When("{string} with id {int} creates a group order")
+    public void creates_a_group_order(String string, Integer int1) {
+        user = registeredUserManager.getRegisteredUserById(int1);
+        assertEquals(string, user.getName());
     }
 
-    @Then("id {int} is created for the group order")
-    public void id_is_created_for_the_group_order(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        groupId = int1;
+    @Then("a group order with id {int} is created")
+    public void a_group_order_with_id_is_created(Integer int1) {
+        nbOfGroupOrderBeforeCreation = groupOrderManager.getGroupOrders().size();
+        groupOrder = new GroupOrderImpl(int1);
+        groupOrderManager.addGroupOrder(int1, groupOrder);
     }
 
-    @Then("{string} chooses a localisation and a delivery time for the group order with id {int}")
-    public void chooses_a_localisation_and_a_delivery_time_for_the_group_order_with_id(String string, Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        deliveryTime = new Date();
-        deliveryLocation = "Templiers";
+    @When("{string} chooses a restaurant, a localisation and a delivery time for the group order with id {int}")
+    public void chooses_a_restaurant_a_localisation_and_a_delivery_time_for_the_group_order_with_id(String string, Integer int1) {
+        assertEquals(string, user.getName());
+        deliveryLocalisation = "Templiers";
+        deliveryTime = new Date(1767225600000L);
+        groupOrder = groupOrderManager.getGroupOrderById(int1);
+        groupOrder.setGroupOrderRestaurant(restaurant);
+        groupOrder.setGroupOrderDeliveryLocation(deliveryLocalisation);
+        groupOrder.setGroupOrderDeliveryDate(deliveryTime);
     }
 
-    @Then("the group with id {int} is created")
-    public void the_group_with_id_is_created(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        groupOrder = new GroupOrderImpl(int1, new Restaurant("nono"), deliveryTime, deliveryLocation);
-        assertEquals(groupOrder.getGroupOrderDeliveryLocation(), deliveryLocation);
-        assertEquals(groupOrder.getGroupOrderDeliveryDate(), deliveryTime);
+    @Then("group order with id {int} restaurant parameter is the one given")
+    public void group_order_with_id_restaurant_parameter_is_the_one_given(Integer int1) {
+        groupOrder = groupOrderManager.getGroupOrderById(int1);
+        assertEquals("Restau", groupOrder.getRestaurant().getName());
     }
+
+    @Then("group order with id {int} localisation parameter is the one given")
+    public void group_order_with_id_localisation_parameter_is_the_one_given(Integer int1) {
+        groupOrder = groupOrderManager.getGroupOrderById(int1);
+        assertEquals("Templiers", groupOrder.getGroupOrderDeliveryLocation());
+    }
+
+    @Then("group order with id {int} delivery time parameter is the one given")
+    public void group_order_with_id_delivery_time_parameter_is_the_one_given(Integer int1) {
+        groupOrder = groupOrderManager.getGroupOrderById(int1);
+        assertEquals(deliveryTime, groupOrder.getGroupOrderDeliveryDate());
+    }
+
+    @When("{string} creates his order for the group order with id {int}")
+    public void creates_his_order_with_id_for_the_group_order_with_id(String string, Integer int1) {
+        assertEquals(string, user.getName());
+        groupOrder = groupOrderManager.getGroupOrderById(int1);
+        order = new Order(new Date(), groupOrder.getGroupOrderDeliveryDate(), groupOrder.getGroupOrderDeliveryLocation(), restaurant);
+        groupOrder.addOrUpdateUserOrder(user, order);
+    }
+
+    @Then("the order restaurant parameter is the one given for group order with id {int}")
+    public void the_order_with_id_restaurant_parameter_is_the_one_given_for_group_order_with_id(Integer int1) {
+        groupOrder = groupOrderManager.getGroupOrderById(int1);
+        order = groupOrder.getOrder(user);
+        assertEquals(groupOrder.getRestaurant().getName(), order.getRestaurant().getName());
+    }
+
+    @Then("the order localisation parameter is the one given for group order with id {int}")
+    public void the_order_with_id_localisation_parameter_is_the_one_given_for_group_order_with_id(Integer int1) {
+        order = groupOrder.getOrder(user);
+        assertEquals(groupOrderManager.getGroupOrderById(int1).getGroupOrderDeliveryLocation(), order.getDeliveryLocation());
+    }
+
+    @Then("the order delivery time parameter is the one given for group order with id {int}")
+    public void the_order_with_id_delivery_time_parameter_is_the_one_given_for_group_order_with_id(Integer int1) {
+        order = groupOrder.getOrder(user);
+        assertEquals(groupOrderManager.getGroupOrderById(int1).getGroupOrderDeliveryDate(), order.getDeliveryDate());
+    }
+
+    @Then("the group order with id {int} is added to the list of group orders")
+    public void the_group_order_with_id_is_added_to_the_list_of_group_orders(Integer int1) {
+        assertEquals(nbOfGroupOrderBeforeCreation+1, groupOrderManager.getGroupOrders().size());
+        assertNotNull(groupOrderManager.getGroupOrderById(int1));
+    }
+
 
 }
