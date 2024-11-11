21cfc86e13075ad56a26654c60b7516a60e4c71d
YohanSI3 <yohan.mazzi@etu.unice.fr>
2024-11-11 12:36:39 +0100

    Ajout des classes nécessaires #45

---
+++ services/Group/src/main/java/fr/unice/polytech/utility/order/GroupOrderProxy.java
@@ -0,0 +1,94 @@
+package fr.unice.polytech.utility.order;
+
+import fr.unice.polytech.utility.restaurant.Restaurant;
+import fr.unice.polytech.utility.user.RegisteredUser;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+public class GroupOrderProxy implements GroupOrderInterface{
+    private final GroupOrderInterface groupOrderInterface;
+    public GroupOrderProxy(GroupOrderInterface groupOrderInterface){
+        this.groupOrderInterface = groupOrderInterface;
+    }
+    @Override
+    public Order getOrder(RegisteredUser user) {
+        return groupOrderInterface.getOrder(user);
+    }
+
+    @Override
+    public int getGroupId() {
+        return groupOrderInterface.getGroupId();
+    }
+
+    @Override
+    public List<RegisteredUser> getUserList() {
+        return null;
+    }
+
+    @Override
+    public void addMember(RegisteredUser user) {
+        return;
+    }
+
+    @Override
+    public Restaurant getRestaurant() {
+        return groupOrderInterface.getRestaurant();
+    }
+
+    @Override
+    public void addOrUpdateUserOrder(RegisteredUser user, Order order) {
+        Restaurant restaurant = groupOrderInterface.getRestaurant();
+        Date groupDeliveryDate = groupOrderInterface.getGroupOrderDeliveryDate();
+        int currentTotalPrepTime = groupOrderInterface.getTotalPreparationTime();
+
+        int newTotalPrepTime = currentTotalPrepTime + order.getTotalPreparationTime(); // Minutes
+        long currentTime = System.currentTimeMillis();
+
+        if (!restaurant.isOpen()) {
+            throw new RuntimeException("Restaurant fermé");
+        }
+
+        if (groupDeliveryDate != null) {
+            long groupDeliveryTime = groupDeliveryDate.getTime(); // Milliseconds
+            long estimatedTimeWithNewOrder = currentTime + (newTotalPrepTime * 60 * 1000); // Milliseconds
+
+            if (estimatedTimeWithNewOrder > groupDeliveryTime) {
+                throw new RuntimeException("Impossible d'ajouter cette commande, elle dépasserait la date de livraison du groupe.");
+            }
+            else{
+                order.updateEstimatedDeliveryDate(estimatedTimeWithNewOrder);
+                groupOrderInterface.addOrUpdateUserOrder(user, order);
+            }
+        }
+
+    }
+
+    @Override
+    public Map<RegisteredUser, Order> getUsersOrders() {
+        return groupOrderInterface.getUsersOrders();
+    }
+
+    @Override
+    public void removeOrder(RegisteredUser user) {
+        groupOrderInterface.removeOrder(user);
+    }
+
+    @Override
+    public int getTotalPreparationTime() {
+        return getUsersOrders().values().stream()
+                .mapToInt(Order::getTotalPreparationTime)
+                .sum();
+    }
+
+    @Override
+    public Date getGroupOrderDeliveryDate() {
+        return groupOrderInterface.getGroupOrderDeliveryDate();
+    }
+
+    @Override
+    public String getGroupOrderDeliveryLocation() {
+        return null;
+    }
+}
