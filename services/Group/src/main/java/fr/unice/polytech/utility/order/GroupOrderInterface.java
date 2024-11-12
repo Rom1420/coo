a6c1da3538f159b9ef357eae5e6fce318b9f89d6
YohanSI3 <yohan.mazzi@etu.unice.fr>
2024-11-12 23:29:03 +0100

    Utilisation du proxy pour les groupes #45

--- services/Group/src/main/java/fr/unice/polytech/utility/order/GroupOrderInterface.java
+++ services/Group/src/main/java/fr/unice/polytech/utility/order/GroupOrderInterface.java
@@ -1,25 +1,28 @@
 package fr.unice.polytech.utility.order;
 
-import fr.unice.polytech.utility.restaurant.Restaurant;
-import fr.unice.polytech.utility.user.RegisteredUser;
-import org.mockito.internal.matchers.Or;
-
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 public interface GroupOrderInterface {
+    void setGroupOrderRestaurant(String restaurant);
+    void setGroupOrderDeliveryDate(Date deliveryDate);
+    void setGroupOrderDeliveryLocation(String deliveryLocation);
+    void addMember(Integer userId);
     Order getOrder(Integer userId);
     String getRestaurant();
     int getGroupId();
     List<Integer> getUserList();
-    void addMember(Integer userId);
     void addOrUpdateUserOrder(Integer userId, Order order);
     Map<Integer, Order> getUsersOrders();
     void removeOrder(Integer userId);
+    int getTotalPreparationTime();
     Date getGroupOrderDeliveryDate();
     String getGroupOrderDeliveryLocation();
-    int getTotalPreparationTime();
+    void validateOrder();
+    String getStatus();
+    void setStatus(String status);
+    void closeOrder();
+    Map<Integer, Integer> calculateOrderHistory();
+    void applyDiscount();
 }
