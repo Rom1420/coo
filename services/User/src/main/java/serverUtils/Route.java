b03ec4c4d9005b1732f675f5215c3d15fb9c38a7
Mathias Santos Reis <mathias.santos-reis@etu.univ-cotedazur.fr>
2024-11-16 13:57:22 +0100

    UserService final

--- services/User/src/main/java/serverUtils/Route.java
+++ services/User/src/main/java/serverUtils/Route.java
@@ -1,2 +1,26 @@
-package serverUtils;public class Route {
+package serverUtils;
+
+public class Route {
+    private final String method;
+    private final String path;
+    private final RouteHandler handler;
+
+    public Route(String method, String path, RouteHandler handler) {
+        this.method = method;
+        this.path = path;
+        this.handler = handler;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public RouteHandler getHandler() {
+        return handler;
+    }
 }
+
--- services/User/src/main/java/serverUtils/RouteHandler.java
+++ services/User/src/main/java/serverUtils/RouteHandler.java
@@ -1,2 +1,10 @@
-package serverUtils;public interface RouteHandler {
+package serverUtils;
+
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+
+@FunctionalInterface
+public interface RouteHandler {
+    void handle(HttpExchange exchange, String id) throws IOException;
 }
+
