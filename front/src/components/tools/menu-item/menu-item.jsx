31236e5e55157b956db12e7e0eadb735f8199d79
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2024-11-22 11:57:48 +0100

    Feat : finished the menu page

--- front/src/components/tools/menu-item/menu-item.jsx
+++ front/src/components/tools/menu-item/menu-item.jsx
@@ -6,6 +6,9 @@ function MenuItem({image, title, smallText}) {
         <img src={image} className='menu-item-pic' alt={title} />
         <h3 className='menu-item-title'>{title}</h3>
         <p className='menu-item-small-text'>{smallText}</p>
+        <div className="details-arrow">
+          <i className="fa-solid fa-arrow-right"></i>
+        </div>
     </div>
   );
 }
