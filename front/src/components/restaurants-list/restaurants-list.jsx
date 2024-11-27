08071515c4b907a2350a60405057a81c11c5a1b7
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2024-11-27 23:26:01 +0100

    feat : restaurant list

---
+++ front/src/components/restaurants-list/restaurants-list.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import Restaurant from '../tools/restaurant/restaurant';
+import './restaurants-list.css';
+
+function RestaurantList({ onBackToHomeClick }) {
+    const [restaurants, setRestaurants] = useState([]);
+
+    useEffect(() => {
+        // Charge les données des restaurants depuis le fichier JSON
+        fetch('/restaurants.json')
+            .then((response) => response.json())
+            .then((data) => setRestaurants(data))
+            .catch((error) => console.error('Erreur lors du chargement des données:', error));
+    }, []);
+
+    // Mise à jour de la couleur du thème
+    const themeColorMetaTag = document.querySelector('meta[name="theme-color"]');
+    themeColorMetaTag.setAttribute('content', '#fff');
+
+    return (
+        <div className="restaurant-container">
+            <i className="fa-solid fa-house" onClick={onBackToHomeClick}></i>
+            <h1 className='restaurant-title'>Liste des Restaurants</h1>
+            <div className="restaurant-list">
+                {restaurants.map((restaurant, index) => (
+                    <Restaurant key={index} restaurant={restaurant} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default RestaurantList;
