22513b075c2963d8521526bcf797f58607200faf
Mathias Santos Reis <mathias.santos-reis@etu.univ-cotedazur.fr>
2024-12-09 12:19:19 +0100

    Feat: front choisir mode paiement

--- front/src/components/cart-resume/cart-resume.jsx
+++ front/src/components/cart-resume/cart-resume.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ReactComponent as ChezPippoLogo} from '../../assets/logo-chez-pippo.svg'
 import './cart-resume.css';
 import RestaurantResume from '../tools/restaurant-resume/restaurant-resume';
 import ArticleList from '../tools/aticle-list/article-list';
 import CheckoutResume from '../checkout-resume/checkout-resume';
 import Button from '../tools/button/button';
+import ProceedPaymentPopUp from "../pop-ups/proceed-payment-pop-up/proceed-payment-pop-up";
 
 function CartResume({ onBackToPrevClick }) {
     const themeColorMetaTag = document.querySelector('meta[name="theme-color"]');
     themeColorMetaTag.setAttribute('content', '#fff');
+
+    const [isProceedPaymentPopUpVisible, setProceedPaymentPopUpVisible] = useState(false);
+    const [isClosing, setIsClosing] = useState(false);
+
+    useEffect(() => {
+        const themeColorMetaTag = document.querySelector('meta[name="theme-color"]');
+
+        if (isProceedPaymentPopUpVisible) {
+            themeColorMetaTag.setAttribute('content', '#6c6c6c'); // Couleur sombre quand ya le pop up
+        } else {
+            themeColorMetaTag.setAttribute('content', '#ffffff'); // Couleur par défaut
+        }
+    }, [isProceedPaymentPopUpVisible]);
+
+    const handleProceedPaymentClick = () => {
+        if (isProceedPaymentPopUpVisible) {
+            setIsClosing(true);
+            setTimeout(() => {
+                setProceedPaymentPopUpVisible(false);
+                setIsClosing(false);
+            }, 300);
+        } else {
+            setProceedPaymentPopUpVisible(true);
+        }
+    };
+
     return (
     <div className="cart-resume-container">
+        {(isProceedPaymentPopUpVisible) && <div className="darker-overlay"></div>}
         <i className="fa-solid fa-arrow-left carte-resume" onClick={onBackToPrevClick}></i>
         <div className="cart-resume-content">
             <h2 className='carte-resume-title'>My Order</h2>
@@ -22,10 +50,12 @@ function CartResume({ onBackToPrevClick }) {
             <ArticleList/>
             <div className="separation-line"></div>
             <CheckoutResume/>
-            <div className="checkout-button-container">
+            {!isProceedPaymentPopUpVisible &&<div className="checkout-button-container">
+                <Button text={"Choose Payment"} style={{width: "20%", marginBottom:4}} onClick={handleProceedPaymentClick}/>
                 <Button text={"Checkout"} style={{width: "20%"}}/>
-            </div>
-        </div> 
+            </div>}
+        </div>
+        {isProceedPaymentPopUpVisible && <ProceedPaymentPopUp onClose={() => {setProceedPaymentPopUpVisible(false)}} closing={isClosing}/>}
     </div>
     );
 }
