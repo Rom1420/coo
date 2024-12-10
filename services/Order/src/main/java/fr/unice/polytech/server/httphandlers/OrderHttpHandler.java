ea4bcf0439c704f3e71285b148c2865f1ac75731
Rom1420 <romain.abbonato@etu.univ-cotedazur.fr>
2024-12-10 23:05:34 +0100

    fix

--- services/Order/src/main/java/fr/unice/polytech/server/httphandlers/OrderHttpHandler.java
+++ services/Order/src/main/java/fr/unice/polytech/server/httphandlers/OrderHttpHandler.java
@@ -9,30 +9,39 @@ import fr.unice.polytech.server.JaxsonUtils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.logging.Logger;
 
 public class OrderHttpHandler implements HttpHandler {
+    private static final Logger logger = Logger.getLogger(OrderHttpHandler.class.getName());
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        String method = exchange.getRequestMethod();
         exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
         exchange.getResponseHeaders().set("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
         exchange.getResponseHeaders().set("Access-Control-Allow-Headers", "Content-Type");
-        String method = exchange.getRequestMethod();
-        switch (method) {
-            case "POST":
-                createOrder(exchange);
-                break;
-            case "GET":
-                getOrder(exchange);
-                break;
-            default:
-                exchange.sendResponseHeaders(405, -1); // Method Not Allowed
+        try {
+            switch (method) {
+                case "POST":
+                    createOrder(exchange);
+                    break;
+                case "GET":
+                    getOrder(exchange);
+                    break;
+                case "OPTIONS":
+                    exchange.sendResponseHeaders(204, -1);
+                    exchange.close();
+                    break;
+                default:
+                    logger.warning("Unknown HTTP method: " + method);
+                    exchange.sendResponseHeaders(405, 0);
+                    exchange.close();
+            }
+        } catch (Exception e) {
+            sendErrorResponse(exchange, 500, "Internal Server Error");
         }
     }
 
     private void createOrder(HttpExchange exchange) throws IOException {
-        exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
-        exchange.getResponseHeaders().set("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-        exchange.getResponseHeaders().set("Access-Control-Allow-Headers", "Content-Type");
         InputStream is = exchange.getRequestBody();
         String requestBody = new String(is.readAllBytes(), StandardCharsets.UTF_8);
 
@@ -46,9 +55,6 @@ public class OrderHttpHandler implements HttpHandler {
     }
 
     private void getOrder(HttpExchange exchange) throws IOException {
-        exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "*");
-        exchange.getResponseHeaders().set("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-        exchange.getResponseHeaders().set("Access-Control-Allow-Headers", "Content-Type");
         String query = exchange.getRequestURI().getQuery();
         int id = Integer.parseInt(query.split("=")[1]);
 
@@ -60,6 +66,14 @@ public class OrderHttpHandler implements HttpHandler {
 
         String response = JaxsonUtils.toJson(order);
         exchange.sendResponseHeaders(200, response.getBytes().length);
+        exchange.getResponseBody().write(response.getBytes());
+        exchange.close();
+    }
+
+    private void sendErrorResponse(HttpExchange exchange, int statusCode, String message) throws IOException {
+        exchange.getResponseHeaders().set("Content-Type", "text/plain");
+        String response = "{\"error\": \"" + message + "\"}";
+        exchange.sendResponseHeaders(statusCode, response.getBytes().length);
         exchange.getResponseBody().write(response.getBytes());
         exchange.close();
     }
